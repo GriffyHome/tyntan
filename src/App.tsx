9eb354bb5ea654a4d94f17fa942bd566651eeeca
sahilpotdukhegriffy <sahilpotdukhe@griffy.app>
2024-10-19 02:32:52 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,276 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { Web3AuthNoModal } from "@web3auth/no-modal";
+import { SolanaPrivateKeyProvider } from "@web3auth/solana-provider";
+import {
+  WALLET_ADAPTERS,
+  CHAIN_NAMESPACES,
+  IProvider,
+  UX_MODE,
+  WEB3AUTH_NETWORK,
+} from "@web3auth/base";
+import { AuthAdapter } from "@web3auth/auth-adapter";
+import "./App.css";
+import RPC from "./evm.viem";
+
+const clientId =
+  "BGuBx89xjIoqSfGJ9lpzFmKERiX0tY0OknUhozpHMwAlV5V22lLrkJjLfpoXmWGXDE4r7L07KfthVV30VtrtfZE";
+
+const chainConfig = {
+  chainNamespace: CHAIN_NAMESPACES.SOLANA,
+  chainId: "0x1",
+  rpcTarget: "https://api.mainnet.solana.com",
+  displayName: "Solana Mainnet",
+  blockExplorerUrl: "https://explorer.solana.com/",
+  ticker: "SOL",
+  tickerName: "Solana",
+};
+
+const privateKeyProvider = new SolanaPrivateKeyProvider({
+  config: { chainConfig },
+});
+
+const web3auth = new Web3AuthNoModal({
+  clientId,
+  web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
+  privateKeyProvider,
+});
+
+const authAdapter = new AuthAdapter({
+  adapterSettings: {
+    uxMode: UX_MODE.REDIRECT,
+    loginConfig: {
+      jwt: {
+        verifier:
+          import.meta.env.VITE_W3A_VERIFIER_NAME || "w3a-telegram-oauth-demo",
+        typeOfLogin: "jwt",
+        clientId,
+      },
+    },
+  },
+});
+web3auth.configureAdapter(authAdapter);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [provider, setProvider] = useState<IProvider | null>(null);
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const jwtToken = params.get("token");
+    if (jwtToken) {
+      loginWithWeb3Auth(jwtToken);
+      window.history.replaceState({}, document.title, window.location.pathname);
+    }
+  }, []);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        await web3auth?.init();
+        setProvider(web3auth?.provider!);
+        if (web3auth?.connected) {
+          setLoggedIn(true);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    init();
+  }, []);
+
+  const login = async () => {
+    const URL =
+      import.meta.env.VITE_SERVER_URL ||
+      "https://w3a-telegram-server.vercel.app";
+    window.location.href = `${URL}/login`;
+  };
+
+  const loginWithWeb3Auth = async (token: string) => {
+    await web3auth?.init();
+
+    const web3authProvider = await web3auth?.connectTo(WALLET_ADAPTERS.AUTH, {
+      loginProvider: "jwt",
+      extraLoginOptions: {
+        id_token: token,
+        verifierIdField: "sub",
+      },
+    });
+    setProvider(web3authProvider!);
+  };
+
+  const authenticateUser = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const idToken = await web3auth.authenticateUser();
+    uiConsole(idToken);
+  };
+
+  const getUserInfo = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const user = await web3auth.getUserInfo();
+    uiConsole(user);
+  };
+
+  const logout = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    await web3auth.logout();
+    setLoggedIn(false);
+    setProvider(null);
+  };
+
+  const getAccounts = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const userAccount = await rpc.getAccounts();
+    uiConsole(userAccount);
+  };
+
+  const getBalance = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const balance = await rpc.getBalance();
+    uiConsole(balance);
+  };
+
+  const getChainId = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const chainId = await rpc.getChainId();
+    uiConsole(chainId);
+  };
+
+  const signMessage = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const result = await rpc.signMessage();
+    uiConsole(result);
+  };
+
+  const sendTransaction = async () => {
+    if (!provider) {
+      uiConsole("provider not initialized yet");
+      return;
+    }
+    const rpc = new RPC(provider);
+    const result = await rpc.sendTransaction();
+    uiConsole(result);
+  };
+
+  function uiConsole(...args: any[]): void {
+    const el = document.querySelector("#console>p");
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
+    }
+  }
+
+  const loginView = (
+    <>
+      <div className="flex-container">
+        <div>
+          <button onClick={getUserInfo} className="card">
+            User Info
+          </button>
+        </div>
+        <div>
+          <button onClick={authenticateUser} className="card">
+            Get ID Token
+          </button>
+        </div>
+        <div>
+          <button onClick={getChainId} className="card">
+            ChainID
+          </button>
+        </div>
+        <div>
+          <button onClick={getAccounts} className="card">
+            Account Address
+          </button>
+        </div>
+        <div>
+          <button onClick={getBalance} className="card">
+            Balance
+          </button>
+        </div>
+        <div>
+          <button onClick={signMessage} className="card">
+            Sign Message
+          </button>
+        </div>
+        <div>
+          <button onClick={sendTransaction} className="card">
+            Send Transaction
+          </button>
+        </div>
+        <div>
+          <button onClick={logout} className="card">
+            Log Out
+          </button>
+        </div>
+      </div>
+
+      <div id="console" style={{ whiteSpace: "pre-line" }}>
+        <p style={{ whiteSpace: "pre-line" }}>Logged in Successfully!</p>
+      </div>
+    </>
+  );
+
+  const logoutView = (
+    <>
+      <button onClick={login} className="card">
+        Login with Telegram
+      </button>
+    </>
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="container">
+      <h1 className="title">
+        <a
+          target="_blank"
+          href="https://web3auth.io/docs/sdk/pnp/web/no-modal"
+          rel="noreferrer"
+        >
+          Web3Auth
+        </a>{" "}
+        & Telegram Login Demo
+      </h1>
+
+      <div className="grid">{loggedIn ? loginView : logoutView}</div>
+
+      <footer className="footer">
+        <a
+          href="https://github.com/Web3Auth/web3auth-pnp-examples/tree/main/web-no-modal-sdk/custom-authentication/single-verifier-examples/telegram-no-modal-example"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source code
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2FWeb3Auth%2Fweb3auth-pnp-examples%2Ftree%2Fmain%2Fweb-no-modal-sdk%2Fcustom-authentication%2Fsingle-verifier-examples%2Ftelegram-no-modal-example&project-name=w3a-telegram-no-modal&repository-name=w3a-telegram-no-modal">
+          <img src="https://vercel.com/button" alt="Deploy with Vercel" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
